1baec3266e844e68b8ffef803c9b9c20b6942dfd
santokie21 <officialuse3012@gmail.com>
2024-04-01 10:38:02 +0530

    Initial Commit

---
+++ src/main/java/io/noobi/carrentalbackend/CarrentalbackendApplication.java
@@ -0,0 +1,13 @@
+package io.noobi.carrentalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrentalbackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CarrentalbackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/io/noobi/carrentalbackend/CarrentalbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.noobi.carrentalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrentalbackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
